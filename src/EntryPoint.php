83ab4211931f7b7e29039789523515e3fe53d3ba
Eric Chau <eric.chau@lp-digital.fr>
2017-09-22 15:11:11 +0200

    purged backbee/backbee-php listeners which are useless in BackBee Cloud context #21411

--- src/EntryPoint.php
+++ src/EntryPoint.php
@@ -2,13 +2,33 @@
 
 namespace BackBeeCloud;
 
+use BackBee\BBApplication;
 use BackBee\Bundle\AbstractBundle;
+use BackBee\Config\Config;
 
 /**
  * @author Eric Chau <eric.chau@lp-digital.fr>
  */
 class EntryPoint extends AbstractBundle
 {
+    /**
+     *
+     *
+     * @param  BBApplication $app
+     * @param  Config        $config
+     */
+    public static function onLoadEvents(BBApplication $app, Config $config)
+    {
+        $app->getConfig()->setSection(
+            'events',
+            $config->getRawSection('events'),
+            true
+        );
+
+        $app->getEventDispatcher()->clearAllListeners();
+        $app->getEventDispatcher()->addListeners($config->getRawSection('events'));
+    }
+
     /**
      * {@inheritdoc}
      */
