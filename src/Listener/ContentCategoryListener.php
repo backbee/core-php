8bbe413122bbd81238f5b23df58f86e3a41162cf
Charles Rouillon <charles.rouillon@lp-digital.fr>
2018-01-05 17:00:57 +0100

    [#22580] Permettre l'ajout de nouvelles catégories de contenu sans surcharge du listener

--- src/Listener/ContentCategoryListener.php
+++ src/Listener/ContentCategoryListener.php
@@ -6,9 +6,66 @@ use BackBee\Controller\Event\PostResponseEvent;
 
 /**
  * @author Eric Chau <eric.chau@lp-digital.fr>
+ * @author Charles Rouillon <charles.rouillon@lp-digital.fr>
  */
 class ContentCategoryListener
 {
+
+    /**
+     * Default ordered content categories.
+     *
+     * @var array
+     */
+    private $defaultData = [
+        'block_category_basics'   => [
+            'pos' => 0,
+            'contents_order' => [
+                'Basic/Title'     => 0,
+                'Text/Paragraph'  => 1,
+                'Media/Image'     => 2,
+                'Text/Button'     => 3,
+                'Media/Video'     => 4,
+                'Basic/Slider'    => 5,
+                'Basic/Searchbar' => 6,
+            ]
+        ],
+        'block_category_pages' => [
+            'pos' => 1,
+        ],
+        'block_category_social' => [
+            'pos' => 2,
+        ],
+        'block_category_more' => [
+            'pos' => 3,
+        ],
+    ];
+
+    /**
+     * If true, only categories in $defaultData will be returned.
+     *
+     * @var boolean
+     */
+    private $strict;
+
+    /**
+     * Listener constructor
+     *
+     * @param array   $data     An ordered categories spec.
+     * @param boolean $override Should the default data overrided? If false (default)
+     *                          $data will be merged with default one.
+     * @param boolean $strict   If true, only categories in $defaultData will be returned.
+     */
+    public function __construct(array $data = [], $override = false, $strict = true)
+    {
+        if (true === boolval($override)) {
+            $this->defaultData = $data;
+        } else {
+            $this->defaultData = array_merge($this->defaultData, $data);
+        }
+
+        $this->strict = boolval($strict);
+    }
+
     /**
      * Re-orders categories to set a custom order.
      *
@@ -16,13 +73,16 @@ class ContentCategoryListener
      */
     public function onGetCategoryPostCall(PostResponseEvent $event)
     {
-        $response = $event->getResponse();
         $result = [];
-        $categoriesData = $this->getCategoriesData();
-        foreach (json_decode($response->getContent(), true) as $data) {
-            $id = $data['id'];
-            if (array_key_exists($id, $categoriesData)) {
-                $config = $categoriesData[$id];
+        $response = $event->getResponse();
+        $decoded = json_decode($response->getContent(), true);
+        $defaultData = $this->getCategoriesData();
+        foreach ($decoded as $data) {
+            // By default the category is pushed at the end of the array
+            $pos = count($result) + count($decoded);
+
+            if (array_key_exists($data['id'], $defaultData)) {
+                $config = $defaultData[$data['id']];
                 if (isset($config['contents_order'])) {
                     $contents = [];
                     foreach ($data['contents'] as $content) {
@@ -36,18 +96,16 @@ class ContentCategoryListener
                     $data['contents'] = $contents;
                 }
 
-                $filteredContents = [];
-                $processedTypes = [];
-                foreach ($data['contents'] as $content) {
-                    if (!in_array($content['type'], $processedTypes)) {
-                        $filteredContents[] = $this->runCustomProcessOnContent($content);
-                        $processedTypes[] = $content['type'];
-                    }
+                if (isset($config['pos'])) {
+                    $pos =intval($config['pos']);
                 }
-
-                $data['contents'] = $filteredContents;
-                $result[$config['pos']] = $data;
+            } elseif ($this->strict) {
+                continue;
             }
+
+            $data['contents'] = $this->filterContents($data['contents']);
+
+            $result[$pos] = $data;
         }
 
         ksort($result);
@@ -62,29 +120,7 @@ class ContentCategoryListener
      */
     protected function getCategoriesData()
     {
-        return [
-            'block_category_basics'   => [
-                'pos' => 0,
-                'contents_order' => [
-                    'Basic/Title'     => 0,
-                    'Text/Paragraph'  => 1,
-                    'Media/Image'     => 2,
-                    'Text/Button'     => 3,
-                    'Media/Video'     => 4,
-                    'Basic/Slider'    => 5,
-                    'Basic/Searchbar' => 6,
-                ]
-            ],
-            'block_category_pages' => [
-                'pos' => 1,
-            ],
-            'block_category_social' => [
-                'pos' => 2,
-            ],
-            'block_category_more' => [
-                'pos' => 3,
-            ],
-        ];
+        return $this->defaultData;
     }
 
     /**
@@ -102,5 +138,26 @@ class ContentCategoryListener
         $content['thumbnail'] = null;
 
         return $content;
+    }
+
+    /**
+     * Filters the set of contents.
+     *
+     * @param  array $contents
+     *
+     * @return array
+     */
+    protected function filterContents($contents)
+    {
+        $filteredContents = [];
+        $processedTypes = [];
+        foreach ($contents as $content) {
+            if (!in_array($content['type'], $processedTypes)) {
+                $filteredContents[] = $this->runCustomProcessOnContent($content);
+                $processedTypes[] = $content['type'];
+            }
+        }
+
+        return $filteredContents;
     }
 }
